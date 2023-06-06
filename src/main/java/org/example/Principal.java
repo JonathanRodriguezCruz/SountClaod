e1493476236d551e6d87afc49065dc2baca8b92a
JonathanRodriguezCruz <jonathan21.rc@gmail.com>
2023-06-06 18:41:15 +0100

    Segundo commit

--- src/main/java/org/example/Principal.java
+++ src/main/java/org/example/Principal.java
@@ -2,20 +2,33 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Principal {
-    private  static Connection db;
+    private static Connection db;
     private static String user = "postgres";
     private static String password = "1234";
 
-    public static void initConection(){
-        try{
-            db = DriverManager.getConnection("jdbc:postgresql://localhost:5432/usuario",user,password);
+    /**
+     * Iniciamos la conexion con la base de datos
+     */
+    public static void initConection() {
+        try {
+            db = DriverManager.getConnection("jdbc:postgresql://localhost:5432/usuario", user, password);
             System.out.println("Exito");
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Noup");
         }
     }
 
-
+    /**
+     * Cerramos la conexion
+     */
+    public static void cerrarConexion() {
+        try {
+            db.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
